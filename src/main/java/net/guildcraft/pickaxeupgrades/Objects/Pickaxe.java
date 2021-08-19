e3c6e67f3cfaf5cf4e72770b22782b73131285fd
Frxq15 <cartwrightdan15@gmail.com>
2021-08-19 20:37:41 +0100

    initial commit

--- src/main/java/net/guildcraft/pickaxeupgrades/Objects/Pickaxe.java
+++ src/main/java/net/guildcraft/pickaxeupgrades/Objects/Pickaxe.java
@@ -1,6 +1,7 @@
 package net.guildcraft.pickaxeupgrades.Objects;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Pickaxe {
     private ItemStack pick;
@@ -8,4 +9,6 @@ public class Pickaxe {
     public Pickaxe(ItemStack pickaxe) {
         pick = pickaxe;
     }
+    public ItemMeta getMeta() { return pick.getItemMeta(); }
+    public String getName() { return getMeta().getDisplayName(); }
 }
